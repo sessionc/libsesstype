20b44f3de35f840a4e24bfdfedf86f95701c0db3
Nicholas Ng <nickng@projectfate.org>
2015-06-25 15:49:56 +0100

    Add implementation and test for Protocol

--- src/api/protocol.cc
+++ src/api/protocol.cc
@@ -1,17 +1,15 @@
-#include <sesstype/constant.h>
+#include <sesstype/import.h>
 #include <sesstype/protocol.h>
+#include <sesstype/role.h>
 
 #ifdef __cplusplus
 namespace sesstype {
 #endif
 
-void st_tree_add_const(st_tree *tree, st_const_t *con);
+st_tree *st_tree_init(st_tree *tree);
+void st_tree_free(st_tree *tree);
 void st_tree_add_import(st_tree *tree, st_tree_import_t import);
 void st_tree_add_role(st_tree *tree, const st_role *role);
-void st_tree_add_role_group(st_tree *tree, const st_role_grp *grp);
-void st_tree_free(st_tree *tree);
-bool st_tree_has_constant(st_tree *tree, const char *name);
-st_tree *st_tree_init(st_tree *tree);
 void st_tree_set_local_name(st_tree *tree, const char *name, const st_role *endpoint_role);
 void st_tree_set_module(st_tree *tree, const char *module);
 void st_tree_set_name(st_tree *tree, const char *name);
