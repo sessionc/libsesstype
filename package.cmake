ab51e4ef20d0d52149435f2daacec05646b2b837
Nicholas Ng <nickng@projectfate.org>
2015-06-29 18:44:11 +0100

    Generate .pc, let CPack use CMAKE's install prefix

--- package.cmake
+++ package.cmake
@@ -1,5 +1,7 @@
 set(CPACK_GENERATOR DEB)
 
+set(CPACK_SET_DESTDIR true) # Use CMAKE_INSTALL_PREFIX
+
 set(CPACK_PACKAGE_NAME ${PROJECT_NAME})
 set(CPACK_PACKAGE_VERSION ${libsesstype_VERSION})
 
@@ -9,7 +11,6 @@ set(CPACK_DEBIAN_PACKAGE_VERSION ${libsesstype_VERSION})
 set(CPACK_DEBIAN_PACKAGE_SECTION "libdevel")
 set(CPACK_DEBIAN_PACKAGE_PRIORITY "optional")
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "Nicholas Ng <nickng@imperial.ac.uk>")
-set(CPACK_DEBIAN_PACKAGE_DESCRIPTION_SUMMARY "Session Types programming library")
 set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Session Types programming library for C/C++.
  This library is a collection of data structures for representing and APIs for manipulating Multiparty Session Types (MPST) for programming.
  A C API is provided for FFI interoperation with other programming languages.
