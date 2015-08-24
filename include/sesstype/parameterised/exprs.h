1055a6dc77e7896d22aeb27fc87962a2bb3bf3cf
Nicholas Ng <nickng@projectfate.org>
2015-08-24 21:13:17 +0100

    Toplevel classes for including all node/expr

---
+++ include/sesstype/parameterised/exprs.h
@@ -0,0 +1,22 @@
+/**
+ * \file sesstype/parameterised/exprs.h
+ * \brief Top-level header to pull in all defined Expr classes in sesstype::parameterised.
+ */
+#ifndef SESSTYPE__PARAMETERISED__EXPRS_H__
+#define SESSTYPE__PARAMETERISED__EXPRS_H__
+
+#include "sesstype/parameterised/expr.h"
+#include "sesstype/parameterised/expr/val.h"
+#include "sesstype/parameterised/expr/var.h"
+#include "sesstype/parameterised/expr/add.h"
+#include "sesstype/parameterised/expr/sub.h"
+#include "sesstype/parameterised/expr/mul.h"
+#include "sesstype/parameterised/expr/div.h"
+#include "sesstype/parameterised/expr/mod.h"
+#include "sesstype/parameterised/expr/shl.h"
+#include "sesstype/parameterised/expr/shr.h"
+#include "sesstype/parameterised/expr/seq.h"
+#include "sesstype/parameterised/expr/rng.h"
+#include "sesstype/parameterised/expr/log.h"
+
+#endif//SESSTYPE__PARAMETERISED__EXPRS_H__
---
+++ include/sesstype/parameterised/nodes.h
@@ -0,0 +1,22 @@
+/**
+ * \file sesstype/parameterised/nodes.h
+ * \brief Top-level header to pull in all defined Node classes in sesstype::parameterised.
+ */
+#ifndef SESSTYPE__PARAMETIERSED__NODES_H__
+#define SESSTYPE__PARAMETIERSED__NODES_H__
+
+#include "sesstype/parameterised/node.h"
+#include "sesstype/parameterised/node/block.h"
+#include "sesstype/parameterised/node/interaction.h"
+#include "sesstype/parameterised/node/choice.h"
+#include "sesstype/parameterised/node/recur.h"
+#include "sesstype/parameterised/node/continue.h"
+#include "sesstype/parameterised/node/par.h"
+#include "sesstype/parameterised/node/nested.h"
+#include "sesstype/parameterised/node/interruptible.h"
+#include "sesstype/parameterised/node/for.h"
+#include "sesstype/parameterised/node/oneof.h"
+#include "sesstype/parameterised/node/if.h"
+#include "sesstype/parameterised/node/allreduce.h"
+
+#endif//SESSTYPE__PARAMETIERSED__NODES_H__
